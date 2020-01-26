b55e8c78b287b2c6fe21cd6439d9c8ec07016cbb
Jacob Westberg <jawes70@gmail.com>
2020-01-26 11:42:01 +0100

    Added outline of window handling

--- src/main.java
+++ src/main.java
@@ -1,9 +1,25 @@
 import javax.swing.*;
 
 public class main {
+    
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Hello world");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    public static void name() {
+        JLabel label = new JLabel("Hello world!");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+    } 
+    public static void main(String[] args) {
         //Entry point
+        SwingUtilities.invokeLater(new Runnable(){
         
+            @Override
+            public void run() {
+                createAndShowGUI();
+            };
+        });
     }
 }
