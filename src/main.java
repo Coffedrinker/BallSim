a86a87ef2b29621e796dc3e00577c09dd2ee40f5
Jacob <jawes70@gmail.com>
2020-01-29 11:36:01 +0100

    Added base grapics and beginning of control.

--- src/main.java
+++ src/main.java
@@ -1,25 +1,54 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class main {
-    //Testing laptop sync
+	
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Hello world");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel("Hello world!");
+        JPanel mainPanel = new MyPanel();
         frame.getContentPane().add(label);
-
-        frame.pack();
+        frame.setMinimumSize(new Dimension(500,200));
+        frame.add(label);
+        frame.add(mainPanel);
+//        frame.pack();
+        
+        
         frame.setVisible(true);
     } 
     public static void main(String[] args) {
         //Entry point
         SwingUtilities.invokeLater(new Runnable(){
         
-            @Override
             public void run() {
                 createAndShowGUI();
             };
         });
     }
+}
+class MyPanel extends JPanel implements ActionListener{
+	int ballX, ballY = 0;
+	
+	public MyPanel(){
+		setMinimumSize(new Dimension(250, 250));
+		setBackground(Color.BLACK);
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		g.drawOval(ballX, ballY, 50, 50);
+	}
 }
