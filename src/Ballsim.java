8f47caa31abc34250998ab28381d7701a1abdc81
Jacob <jawes70@gmail.com>
2020-03-03 20:39:08 +0100

    Angular collisions work. Normals do not

--- src/Ballsim.java
+++ src/Ballsim.java
@@ -1,13 +1,8 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.Polygon;
-import java.awt.geom.Point2D;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import javax.swing.*;
 
 public class Ballsim {
@@ -19,7 +14,7 @@ public class Ballsim {
         JLabel label = new JLabel("Hello world!");
         MyPanel mainPanel = new MyPanel();
         frame.getContentPane().add(label);
-        frame.setMinimumSize(new Dimension(300,400));
+        frame.setMinimumSize(new Dimension(300,1020));
         frame.add(label);
         frame.add(mainPanel);
 //        frame.pack();
@@ -45,6 +40,7 @@ class Obstacle {
 	private int posX, posY, width, height;
 	private double rotation;
 	private Polygon shape;
+	private int[] listX, listY;
 	public Obstacle() {	
 		// TODO Auto-generated constructor stub
 		this.posX = 0;
@@ -70,8 +66,8 @@ class Obstacle {
 		return this.shape;
 	}
 	private Polygon makeShape(){
-		int[] listX = {0,width, width,0}; 
-		int[] listY = {0,0,height, height};
+		listX = new int[] {0,width, width,0}; 
+		listY = new int[] {0,0,height, height};
 		
 		for (int i = 0; i < listY.length; i++) {
 			int[] newPoint = rotatePoint(listX[i], listY[i], rotation);
@@ -112,14 +108,22 @@ class Obstacle {
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-	public boolean[] getRelativePositionOf(Ball b){		
+	public boolean[] getRelativePositionOf(Ball b){	
+		 
 		boolean north = (int) (b.getPosY() + b.getSize()) <= (int) this.posY;
 		boolean south = (int) b.getPosY() > (int) (this.posY + this.height);
 		boolean west = (int) (b.getPosX() + b.getSize()) < (int) this.posX;
-		boolean east = (int) b.getPosX() > (int) (this.posX + this.width);
+		boolean east = (int) b.getPosX() > (int) (this.posX + this.width);			
+		if (this.rotation != 0){
+			north = rotatePoint((int) b.getPosX(),(int) b.getPosY(), -this.rotation)[1] + b.getSize()<= (int) this.posY;
+		}
 		
 		boolean[] direction = {north,south,west,east};
 		return direction;
+	}
+	public int[][] getPoints(){
+		int[][] points = new int[][] {listX,listY};
+		return points;
 	}
 }
 
@@ -128,8 +132,8 @@ class Ball {
 	private int size;
 	
 	public Ball() {
-		this.posX = 0;
-		this.posY = 0;
+		this.posX = 1;
+		this.posY = 1;
 		this.size = 20;
 		this.velocityX = 100;
 		this.velocityY = 0;
@@ -186,22 +190,17 @@ class Ball {
 
 class MyPanel extends JPanel implements Runnable{
 	//1000f = 1m
-//	float ballX = 1f; 
-//	float ballY = 1f;
-//	float ballVelocityX = 100f;
-//	float ballVelocityY = 0f;
-//	int ballSize = 20;
 	float gravity = 9800f;
-	float studsKoeff = -0.4f;
-	float timeScale = 0.2f;
+	float studsKoeff = -1f;
+	float timeScale = 1f;
 	
 	long timer, timeSinceLastFrame, startTime;
 	int fps, accumulator = 0;
 	
 	private boolean running = true;
 	
-	ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
-	Ball ball;		
+	private ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
+	private Ball ball;		
 	
 	public MyPanel(){
 		setMinimumSize(new Dimension(250, 250));
@@ -228,33 +227,64 @@ class MyPanel extends JPanel implements Runnable{
 //				(int) (ball.getPosY() + ball.getVelocityY()* timeScale * timeSinceLastFrame / 1000),
 //						ball.getSize(),
 //						ball.getSize());
+		g.drawLine((int) ball.getPosX(), (int) ball.getPosY(),(int)  ball.getPosX() + (int) ball.getVelocityX(), (int) ball.getVelocityY());
 		fps ++;
 	}
 	public synchronized void doStop(){
 		this.running = false;
 	}
+	// Beräkna skärande punkt hos 2 linjer utifrån 2 punkter från dessa linjer
+	// a -> b och c -> d
+	// TODO: Byt namn!
+	public double[] getIntersectingPoint(double ax, double ay, double bx, double by, double cx, double cy, double dx, double dy){
+		double fakAToB = (by-ay)/(bx-ax);
+		double fakCToD = (dy-cy)/(dx-cx);
+		
+		double konstAToB = ay-fakAToB*ax;
+		double konstCToD = cy-fakCToD*cx;
+		
+		double pointX = (konstCToD-konstAToB)/(fakAToB-fakCToD); // ?!
+		
+		// Räta linjens ekvation f= k*x + m
+		double pointY = fakAToB*pointX + konstAToB;
+				
+		double newAngle = Math.atan(fakAToB) - Math.atan(fakCToD);
+		double origLength = Math.sqrt((dx-cx)*(dx-cx)+(dy-cy)*(dy-cy));
+//		double clipLength = origLength - Math.sqrt((pointX-cy)*(pointX-cy)+(pointY-cy)*(pointY-cy));
+
+		double newPointX = pointX + origLength * Math.cos(newAngle);
+		double newPointY = pointY + origLength * Math.sin(newAngle);
+		double newVelocityX = origLength * Math.cos(newAngle);
+		double newVelocityY = origLength * Math.sin(newAngle);
+		return new double[] {newPointX,newPointY,newVelocityX, newVelocityY};
+	}
+	
 	public void run() {
 		// XXX: Initialize world
 		startTime = System.currentTimeMillis();
 		timer = System.currentTimeMillis();
+		timeSinceLastFrame = 2;
 		
 		int north = 0, south = 1, west = 2, east = 3 ;
-		timeSinceLastFrame = 2;
+		boolean collisionNorth;
+		double[] newPosAndAngle;
+		boolean[] direction;
 		
 		obstacles.add( new Obstacle(0, this.getHeight()+1, this.getWidth(), 500, 0));
 		obstacles.add( new Obstacle(this.getWidth()+1, 0, 20, this.getHeight(), 0));
 		obstacles.add( new Obstacle(-30, 0, 30, this.getHeight(), 0));
-		obstacles.add( new Obstacle(0, 129, 80, 200, 0));
-		obstacles.add( new Obstacle(0, 100, 100, 30, 30));
+//		obstacles.add( new Obstacle(0, 129, 80, 200, 0));
+		obstacles.add( new Obstacle(0, 130, 150, 30, 30));
+		
 		
 //		Point2D cp = new Point((int) ball.getPosX(), (int) ball.getPosY()); 
 		while(this.running) {
-			boolean collisionNorth = false;
 //			timeSinceLastFrame = System.currentTimeMillis() - timer;
 //			timer = System.currentTimeMillis();
 //			System.out.println(timer);
 //			cp.setLocation((int) ballX + ballSize/2, (int) ballY + ballSize/2);
 			
+			collisionNorth = false;
 			// XXX: Handle collisions
 			for (Obstacle obs : obstacles) { 
 				if (obs.getShape().intersects(
@@ -262,18 +292,25 @@ class MyPanel extends JPanel implements Runnable{
 						ball.getPosY() + ball.getVelocityY() * timeScale * timeSinceLastFrame / 1000,
 						ball.getSize(),
 						ball.getSize())) {
-
-					boolean[] direction = obs.getRelativePositionOf(ball); 
 					
+					direction = obs.getRelativePositionOf(ball); 
 					if (direction[north] && !direction[west] && !direction[east]) {
-						
+						newPosAndAngle = getIntersectingPoint(
+								obs.getPoints()[0][0], 
+								obs.getPoints()[1][0], 
+								obs.getPoints()[0][1], 
+								obs.getPoints()[1][1], 
+								ball.getPosX() , 
+								ball.getPosY() + ball.getSize(),
+								ball.getPosX() + ball.getVelocityX() * timeScale * timeSinceLastFrame /1000, 
+								ball.getPosY() + ball.getSize() + ball.getVelocityY() * timeScale * timeSinceLastFrame /1000);
+						System.out.println(ball.getVelocityY() + "mm/s");
 						collisionNorth = true;
-						ball.setPosY(obs.getPosY() - ball.getSize());
-						if (ball.getVelocityY() > gravity*timeScale*timeSinceLastFrame /1000) {
-							ball.setVelocityY(ball.getVelocityY() * studsKoeff);
-						}else{
-							ball.setVelocityY(0);
-						}
+						ball.setPosX((int) newPosAndAngle[0]);
+						ball.setPosY((int) newPosAndAngle[1] - ball.getSize());
+						
+						ball.setVelocityX((int) newPosAndAngle[2]/(timeScale * timeSinceLastFrame / 1000));
+						ball.setVelocityY((int) newPosAndAngle[3]/(timeScale * timeSinceLastFrame / 1000));
 					}
 					if (direction[west] && !direction[north] && !direction[south]) {
 						ball.setVelocityX(ball.getVelocityX() * studsKoeff);
@@ -291,19 +328,19 @@ class MyPanel extends JPanel implements Runnable{
 			ball.setPosY(ball.getPosY() + ball.getVelocityY()*timeScale*timeSinceLastFrame/1000);
 			
 			// XXX: Draw simulation and start next frame
+			accumulator += timeSinceLastFrame * timeScale;
+			if (accumulator >= 1000){
+				System.out.println(fps +" | "+ timeSinceLastFrame);
+				accumulator -= 1000;
+				fps=0;
+				System.out.println(ball.getVelocityY() + "mm/s");
+//				ball.setPosX(0);
+//				ball.setPosY(0);
+			}
+			revalidate();
+			repaint();
 			try {
-				accumulator += timeSinceLastFrame;
-				if (accumulator >= 1000){
-					System.out.println(fps +" | "+ timeSinceLastFrame);
-					accumulator -= 1000;
-					fps=0;
-//					ball.setPosX(0);
-//					ball.setPosY(0);
-				}
-				Thread.sleep(1); // lag simulering, om tid mellan rutor är < 1 ms så blinkar grafiken
-				
-				revalidate();
-				repaint();
+				Thread.sleep(2); 
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
